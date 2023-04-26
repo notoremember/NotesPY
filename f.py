f83b383243b9cf75392b4a96c61844460f5b503e
Daniil <ryazapovdanii@gmail.com>
2023-04-26 15:50:35 +0300

    функции во втором классе: чтение , создание , редактирование , выход

--- f.py
+++ f.py
@@ -35,3 +35,45 @@ class NoteManager:
                     updated = datetime.datetime.strptime(row[4], "%Y-%m-%d %H:%M:%S")
                     note = Note(id, title, body, created, updated)
                     self.notes.append(note)
+                    
+    def save_notes(self):
+        with open(self.filename, "w", newline="") as f:
+            writer = csv.writer(f, delimiter=";")
+            for note in self.notes:
+                writer.writerow([note.id, note.title, note.body, note.created, note.updated])
+
+    def create_note(self, title, body):
+        id = max([note.id for note in self.notes]) + 1 if self.notes else 1
+        created = datetime.datetime.now()
+        updated = datetime.datetime.now()
+        note = Note(id, title, body, created, updated)
+        self.notes.append(note)
+        return note
+
+    def read_notes(self):
+        return self.notes
+
+    def get_note_by_id(self, id):
+        for note in self.notes:
+            if note.id == id:
+                return note
+        return None
+
+    def update_note(self, id, title=None, body=None):
+        note = self.get_note_by_id(id)
+        if note:
+            note.title = title if title is not None else note.title
+            note.body = body if body is not None else note.body
+            note.updated = datetime.datetime.now()
+            return note
+        else:
+            return None
+
+
+
+
+    def delete_note(self, id):
+        note = self.get_note_by_id(id)
+        if note:
+            self.notes.remove(note)
+
